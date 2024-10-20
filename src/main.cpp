889f7ee14a902426aa976627ebac065479f3bc2c
BenjaminAGH <benjamin.guajardo2101@alumnos.ubiobio.cl>
2024-10-20 14:12:34 -0300

    Entrada consola y meta

--- src/main.cpp
+++ src/main.cpp
@@ -31,9 +31,10 @@ carrera.
 
 using namespace std;
 
-const int Meta = 50;
+int Meta = 0;
+int N = 0;
 
-void carrera(car &auto_ref){
+void carrera(car &auto_ref, vector<car> &posicionesAuto) {
     while(auto_ref.get_distancia_total() < Meta) {
         int distancia = auto_ref.avanzar(1);
         if(distancia == 0) {
@@ -44,20 +45,44 @@ void carrera(car &auto_ref){
         }
     }
     cout << "\033[32mAuto " << auto_ref.get_id() << " ha llegado a la meta\033[0m" << endl;
+    posicionesAuto.push_back(auto_ref);
 }
 
-int main(){
-    srand(time(0));
-    const int N = 5;
+void posiciones(vector<car> &posicionesAuto) {
+    cout << endl;
+    cout << "Lugar     Auto" << endl;
+    cout << "───────────────" << endl;
+    for(size_t i = 0; i < posicionesAuto.size(); ++i) {
+        car auto_ref = posicionesAuto[i];
+        cout << i + 1 << "        ";
+        cout << "Auto" << auto_ref.get_id() << endl; 
+    }
+}
 
+int main(int argc, char *argv[]) {
+    srand(time(0));
+
+    if (argc < 3) {
+        cout << "Uso: " << argv[0] << " <distancia_total> <numero_autos>" << endl;
+        return 1;
+    }
+
+    Meta = atoi(argv[1]);
+    N = atoi(argv[2]);
+
+    vector<car> tablaPosiciones;
     vector<car> autos;
+
+    cout << "\033[35mDistancia total de CARRERA: " << Meta << " metros\033[0m" << endl;
+    cout << "\033[35m──────────────────────────────────────\033[0m" << endl;    
+
     for(int i = 1; i <= N; ++i) {
         autos.emplace_back(i);
     }
 
     vector<thread> threads;
     for(int i = 0; i < N; ++i) {
-        threads.push_back(thread(carrera, ref(autos[i])));
+        threads.push_back(thread(carrera, ref(autos[i]), ref(tablaPosiciones)));
     }
 
     for(auto& t : threads) {
@@ -65,7 +90,7 @@ int main(){
     }
 
     cout << "\033[33mLa carrera ha terminado!\033[0m" << endl;
-    return 0;
+    posiciones(tablaPosiciones);
 
-  return 0;
+    return 0;
 }
